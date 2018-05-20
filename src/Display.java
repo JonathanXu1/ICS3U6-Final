bb7496b98e8253cdbc32f2335fdc2b021ca037b3
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-05-20 12:40:06 -0400

    Visual changes

--- src/Display.java
+++ src/Display.java
@@ -19,11 +19,16 @@ class Display extends JFrame{
     this.maxX= (int)(Toolkit.getDefaultToolkit().getScreenSize().getWidth());
     this.maxY = (int)(Toolkit.getDefaultToolkit().getScreenSize().getHeight());
     this.setSize(maxX, maxY);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
+  public void menu(){
+  }
+  public void start(){
     polygon =3;
     botPanel = new DisplayPanel(maxX, (int)(maxY*2.0/10.0), Color.GRAY, polygon);
     polygon =0;
-    gamePanel = new DisplayPanel(maxX, (int)(maxY*9.0/10.0), Color.BLACK, polygon);
+    gamePanel = new DisplayPanel(maxX, (int)(maxY*9.0/10.0), Color.WHITE, polygon);
     this.setLayout(new BorderLayout());
     this.add (botPanel, BorderLayout.SOUTH);
     this.add (gamePanel, BorderLayout.NORTH);
@@ -36,12 +41,9 @@ class Display extends JFrame{
     rightPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
     polygon =1;
     hpPanel = new DisplayPanel((int)(maxX*1.5/5.0),(int)(maxY*1.0/20.0), Color.RED, polygon);
-    expPanel = new DisplayPanel((int)(maxX*1.0/4.0),(int)(maxY*1.0/30.0), Color.BLACK, polygon);
+    expPanel = new DisplayPanel((int)(maxX*1.0/4.0),(int)(maxY*1.0/30.0), Color.BLUE, polygon);
     rightPanel.add (hpPanel);
     rightPanel.add (expPanel);
-    this.setVisible(true);
-  }
-  public void start(){
     gamePanel.animate();
     botPanel.animate();
     mapPanel.animate();
