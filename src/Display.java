2bff7f82734c4a47f44ae5529033708dc32706b1
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-05-20 13:36:37 -0400

    Organization

--- src/Display.java
+++ src/Display.java
@@ -23,19 +23,20 @@ class Display extends JFrame{
     this.setVisible(true);
   }
   public void menu(){
+    
   }
   public void start(){
+    this.setLayout(new BorderLayout());
     polygon =3;
     botPanel = new DisplayPanel(maxX, (int)(maxY*2.0/10.0), Color.GRAY, polygon);
     polygon =0;
     gamePanel = new DisplayPanel(maxX, (int)(maxY*9.0/10.0), Color.WHITE, polygon);
-    this.setLayout(new BorderLayout());
     this.add (botPanel, BorderLayout.SOUTH);
     this.add (gamePanel, BorderLayout.NORTH);
     gamePanel.setLayout(new BorderLayout());
+    polygon =0;
     mapPanel = new DisplayPanel((int)(maxX*1.0/5.0),(int)(maxX*1.0/5.0), Color.GRAY, polygon);
     rightPanel = new DisplayPanel((int)(maxX*1.5/5.0),(int)(maxY*1.0/20.0), Color.WHITE, polygon);
-    polygon =0;
     gamePanel.add (mapPanel, BorderLayout.EAST);
     gamePanel.add (rightPanel, BorderLayout.WEST);
     rightPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
