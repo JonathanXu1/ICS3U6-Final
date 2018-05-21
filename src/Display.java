6207ccc0e7714bf73e2a27ca7252938a08d74500
Jonathan Xu <jonathanlikestoemail@gmail.com>
2018-05-20 21:03:56 -0400

    A brief description goes here

--- src/Display.java
+++ src/Display.java
@@ -10,6 +10,7 @@ class Display extends JFrame{
   private DisplayPanel hpPanel;
   private DisplayPanel expPanel;
   private DisplayPanel accessoryPanel;
+  private DisplayPanel menuPanel;
   private int polygon =0;
   private int maxX;
   private int maxY;
@@ -23,7 +24,17 @@ class Display extends JFrame{
     this.setVisible(true);
   }
   public void menu(){
-    
+    this.setLayout(new BorderLayout());
+    JLabel title = new JLabel("CONCORDIA");
+    menuPanel = new DisplayPanel(300, 300, Color.GRAY, 0);
+    this.add(menuPanel, BorderLayout.WEST);
+    menuPanel.setLayout(new GridLayout(5, 1));
+    menuPanel.add(new Button("Continue"));
+    menuPanel.add(new Button("New"));
+    menuPanel.add(new Button("Load"));
+    menuPanel.add(new Button("Settings"));
+    menuPanel.add(new Button("Scoreboard"));
+    menuPanel.animate();
   }
   public void start(){
     this.setLayout(new BorderLayout());
--- src/DisplayTest.java
+++ src/DisplayTest.java
@@ -1,6 +1,13 @@
 class DisplayTest{
   public static void main (String[] args){
     Display disp = new Display ();
+    disp.menu();
+    //Delay to test out transition; will delete later
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     disp.start();
   }
 }
