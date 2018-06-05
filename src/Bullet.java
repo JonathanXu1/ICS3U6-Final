3c18ca33d4e3629c1c0e9d0a163db20b8307c494
Jonathan <jonathanxu25@gmail.com>
2018-06-05 14:42:51 -0400

    Created bullet class

---
+++ src/Bullet.java
@@ -0,0 +1,10 @@
+public class Bullet{
+  int  startX, startY, endX, endY;
+  double angle;
+  Bullet(int startX, int startY, int endX, int endY){
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+  }
+}
