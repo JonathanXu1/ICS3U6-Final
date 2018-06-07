fef4332c8152001b2d71710b970e6958f89dfe96
Code-Titan <artemis.sotnikov12@gmail.com>
2018-06-07 14:27:23 -0400

    Reworked Melee weapons to have graphics

--- src/NuclearHammer.java
+++ src/NuclearHammer.java
@@ -1,8 +1,14 @@
-class NuclearHammer {
+import java.awt.Graphics;
+
+class NuclearHammer extends MeleeWeapon{
   NuclearHammer(int dbty) {
     super(dbty);
     
     this.setRarity(8);
     this.setDamage(100);
+  }
+  
+  public void drawItem(Graphics g, int x, int y, int width, int height, GamePanel gamePanel){
+  
   }
 }
--- src/PlasmaRapier.java
+++ src/PlasmaRapier.java
@@ -1,7 +1,13 @@
+import java.awt.Graphics;
+
 public class PlasmaRapier extends MeleeWeapon {
   PlasmaRapier(int dbty) {
     super(dbty);
     this.setRarity(150);
     this.setDamage(20);   
+  }
+  
+  public void drawItem(Graphics g, int x, int y, int width, int height, GamePanel gamePanel) {
+  
   }
 }
