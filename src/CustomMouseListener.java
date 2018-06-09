94b289bbd049d2821184e8b3de570398d2fdb26a
Will Jeong <willdjeong@gmail.com>
2018-06-09 17:02:38 -0400

    Changed mouse

--- src/CustomMouseListener.java
+++ src/CustomMouseListener.java
@@ -5,9 +5,8 @@ import java.awt.event.MouseEvent;
 class CustomMouseListener implements MouseListener {
   //Controls the x and y position of the mouse
   private int[] xy = new int [2];
-  private boolean alternateButton = false;
   //The following booleans control the state of the mouse
-  private boolean released, hover;
+  private boolean pressed, hover;
   private boolean inventoryMove = false;
   
   //Methods that are implemented from MouseListener
@@ -16,12 +15,10 @@ class CustomMouseListener implements MouseListener {
   public void mousePressed(MouseEvent e) {
     xy[0] = e.getX();
     xy[1] = e.getY();
-    released = false;
-    this.alternateButton = true;
+    pressed = true;
   }
   public void mouseReleased(MouseEvent e) {
-    released = true;
-    this.alternateButton = false;
+    pressed = false;
   }
   public void mouseEntered(MouseEvent e) {
     hover = true;
@@ -37,19 +34,13 @@ class CustomMouseListener implements MouseListener {
     return xy;
   }
   //There is no setter for this as it can only be controlled by the mouse
-  public void setReleased(boolean released){
-    this.released = released;
-  }
-  public boolean getReleased(){
-    return released;
+  public boolean getPressed(){
+    return pressed;
   }
   public boolean getHover(){
     return hover;
   }
   //There is no setter for this as it can only be controlled by the mouse
-  public boolean getAlternateButton(){
-    return (alternateButton);
-  }
   public void resetXY(){
     xy[0] =0;
     xy[1] = 0;
