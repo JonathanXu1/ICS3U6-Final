a85c02ff0e92d9a86f81a7f0bb08cf616f9dd46f
Jonathan <jonathanxu25@gmail.com>
2018-06-12 18:21:20 -0400

    slightly better menu stars

--- src/MenuBGPanel.java
+++ src/MenuBGPanel.java
@@ -8,7 +8,7 @@ import java.util.Random;
 class MenuBGPanel extends JPanel{
   private int xVal;
   private int yVal;
-  private int pixelX, pixelY;
+  private double pixelX, pixelY;
   private int starCount;
   private Image menuBg = Toolkit.getDefaultToolkit().getImage("../res/bg.png");
   private Star[][] stars = new Star[100][200];
@@ -24,14 +24,14 @@ class MenuBGPanel extends JPanel{
     this.setLayout(null);
     
     pixelX = xVal/200;
-    pixelY = yVal/100;
+    pixelY = yVal/100; 
   }
   @Override
   public void paintComponent(Graphics g){
     count ++;
     super.paintComponent(g);
     g.drawImage(menuBg,0,0,xVal,yVal,this);
-    if(count >= 10){ //Display count for buffering animations
+    if(count >= 20){ //Display count for buffering animations
       count = 0; 
       starCount = 1; //# stars
       for (int i = 0; i <= starCount; i ++){
@@ -46,11 +46,20 @@ class MenuBGPanel extends JPanel{
           if(stars[i][j] != null){
             if(!stars[i][j].getDied()){
               stars[i][j].updateColor();
-              g.setColor(stars[i][j].getColor());
-              g.fillRect(j*pixelX, i*pixelY, pixelX, pixelY);
-            } else{
-              stars[i][j] = null;
             }
+          }
+        }
+      }
+    }
+    
+    for (int i = 0; i < stars.length; i ++){
+      for (int j = 0; j < stars[0].length; j++){
+        if(stars[i][j] != null){
+          if(!stars[i][j].getDied()){
+            g.setColor(stars[i][j].getColor());
+            g.fillRect((int)(j*pixelX), (int)(i*pixelY), (int)(pixelX), (int)(pixelY));
+          } else {
+            stars[i][j] = null;
           }
         }
       }
--- src/Star.java
+++ src/Star.java
@@ -2,14 +2,15 @@ import java.util.Random;
 import java.awt.Color;
 
 public class Star{
-  private Color color;
   private int rgb, maxrgb;
+  private int minrgb = 21;
+  private Color color = new Color(minrgb, minrgb, minrgb);
   private boolean peaked = false;
   private boolean died = false;
   Random rand = new Random();
   Star(){
-    color = new Color(0, 0, 0);
-    maxrgb = rand.nextInt(100)+150; //150 to 250
+    maxrgb = rand.nextInt(100)+75; //75 to 175
+    rgb = minrgb;
   }
   public Color getColor(){
     return color;
@@ -17,12 +18,12 @@ public class Star{
   public void updateColor(){
     if(rgb <= maxrgb && !peaked){
       rgb += 5;
-    } else if(rgb > maxrgb){
+    } else if(rgb > maxrgb && !peaked){
       peaked = true;
     }
-    if (rgb >= 5 && peaked){
+    if (rgb >= minrgb && peaked){
       rgb -= 5;
-    } else if (rgb < 5 && peaked){
+    } else if (rgb < minrgb && peaked){
       died = true;
     }
     color = new Color(rgb, rgb, rgb);
