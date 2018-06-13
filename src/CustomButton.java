6dbf37ca8202f44ea70ea2b2e2b15b0fa0392d61
Jonathan <jonathanxu25@gmail.com>
2018-06-13 10:43:42 -0400

    Added custom font to buttons and revamped organization

--- src/CustomButton.java
+++ src/CustomButton.java
@@ -2,19 +2,37 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import java.io.File;
+import java.awt.FontFormatException;
+import java.awt.Color;
+import java.io.IOException;
 
 class CustomButton extends JButton{
-  private JLabel name;
+  private JLabel label;
+  private Font customHeader;
+  
   //Constructor, no methods required
-  CustomButton(String name, int type){
-    this.name = new JLabel(name);
+  CustomButton(String name, int type, CustomMouseListener listener){
+    label = new JLabel(name);
+    try {
+      customHeader = Font.createFont(Font.TRUETYPE_FONT, new File("../res/fonts/spaceage.ttf")).deriveFont(32f);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch(FontFormatException e) {
+      e.printStackTrace();
+    }
+    this.addMouseListener(listener);
+    label.setForeground(Color.WHITE);
+    label.setFont(customHeader);
+    this.add(label);
+    
     if(type == 0){
       //Transparent button
       this.setBorderPainted(false);
       this.setFont(new Font("sansserif", Font.BOLD, 20));
       //this.setAlignmentX(0);
       this.setLayout(new BorderLayout());
-      this.add(this.name, BorderLayout.WEST);
+      this.add(label, BorderLayout.WEST);
     }
   }
 }
--- src/Display.java
+++ src/Display.java
@@ -15,13 +15,6 @@ class Display extends JFrame{
   private boolean addGamePanel = false;
   private int maxX, maxY;
   //Menu
-  //Buttons
-  private CustomButton continueButton = new CustomButton("Continue", 0);
-  private CustomButton newGameButton = new CustomButton("New", 0);
-  private CustomButton loadGameButton = new CustomButton("Load", 0);
-  private CustomButton settingsButton = new CustomButton("Settings", 0);
-  private CustomButton scoreboardButton = new CustomButton("Scoreboard", 0);
-  private CustomButton quitButton = new CustomButton("Quit", 0);
   //The components of the menu
   private MenuPanel menuPanel;
   private MenuBGPanel menuBgPanel;
@@ -37,12 +30,19 @@ class Display extends JFrame{
   private CustomMouseListener settingsButtonMouse = new CustomMouseListener();
   private CustomMouseListener scoreboardButtonMouse = new CustomMouseListener();
   private CustomMouseListener quitButtonMouse = new CustomMouseListener();  
+  //Buttons
+  private CustomButton continueButton = new CustomButton("Continue", 0,continueButtonMouse);
+  private CustomButton newGameButton = new CustomButton("New", 0,newGameButtonMouse);
+  private CustomButton loadGameButton = new CustomButton("Load", 0,loadGameButtonMouse);
+  private CustomButton settingsButton = new CustomButton("Settings", 0, settingsButtonMouse);
+  private CustomButton scoreboardButton = new CustomButton("Scoreboard", 0, scoreboardButtonMouse);
+  private CustomButton quitButton = new CustomButton("Quit", 0, quitButtonMouse);
   //Game logic
   private Tile[][] map;
   private int playerStartingX;
   private int playerStartingY;
   //Fonts
-  Font customTitle, customHeader;
+  Font customTitle;
   
   Display(){
     super ("Concordia");
@@ -56,7 +56,6 @@ class Display extends JFrame{
     //Adds fonts
     try {
       customTitle = Font.createFont(Font.TRUETYPE_FONT, new File("../res/fonts/spaceage.ttf")).deriveFont(80f);
-      customHeader = Font.createFont(Font.TRUETYPE_FONT, new File("../res/fonts/spaceage.ttf")).deriveFont(32f);
     } catch (IOException e) {
       e.printStackTrace();
     } catch(FontFormatException e) {
@@ -79,14 +78,7 @@ class Display extends JFrame{
     
     //Menu panel and buttons
     menuPanel = new MenuPanel(50, maxY/2, 300, 220);
-    menuPanel.setFont(customHeader);
-    
-    continueButton.addMouseListener(continueButtonMouse);
-    newGameButton.addMouseListener(newGameButtonMouse);
-    loadGameButton.addMouseListener(loadGameButtonMouse);
-    settingsButton.addMouseListener(settingsButtonMouse);
-    scoreboardButton.addMouseListener(scoreboardButtonMouse);
-    quitButton.addMouseListener(quitButtonMouse);
+    //menuPanel.setFont(customHeader);
     
     //Adds everything
     menuPanel.add(continueButton);
