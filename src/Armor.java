7e49cc8cb9b18b85ae67e5898ab0a6a6003bf6bc
Code-Titan <artemis.sotnikov12@gmail.com>
2018-06-14 02:18:45 -0400

    Set up comments + removed gameSaver components

--- src/Armor.java
+++ src/Armor.java
@@ -2,14 +2,17 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+
 public abstract class Armor extends Equipment {
   private int defense;
   private int freezeDefense;
   private int flameDefense;
   private int lightningDefense;
   Armor(int dbty){
-    super (dbty);
+    super (dbty); // Passes durability to Equipment superclass
   }
+  
+  // Getters-Setters
   public int getDefense(){
     return (defense);
   }
@@ -34,5 +37,7 @@ public abstract class Armor extends Equipment {
   public void setLightningDefense(int lightningDefense){
     this.lightningDefense = lightningDefense;
   }
+  
+  // Abstract method for drawing the object, taking in an x,y dimensions, and the gamepanel to be applied to. 
   abstract public void drawItem(Graphics g, int x, int y, int width, int height, GamePanel gamePanel);
 }
